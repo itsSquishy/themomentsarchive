cc6ad75c554610ecbf023a3d477f6756c1b1ce94
itsSquishy <itssquishy@gmail.com>
2024-10-02 22:03:41 +0800

    added netlify url in environment variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,13 @@ import "./globals.css";
 import Navbar from "@/components/navbar";
 import { Toaster } from "@/components/ui/sonner"
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+// const defaultUrl = process.env.VERCEL_URL
+//   ? `https://${process.env.VERCEL_URL}`
+//   : "http://localhost:3000";
+
+const defaultUrl = process.env.NETLIFY_URL
+? `https://${process.env.NETLIFY_URL}`
+: "http://localhost:3000";
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
